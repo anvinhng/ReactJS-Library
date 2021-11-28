97b0d91c97fc8c90ead401b23be3fa0f13842ba2
Vinh An <babaiu18001@student.hcmiu.edu.vn>
2021-11-28 18:43:48 +0700

    add exercises 1-2-3 lab01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Exercise02 from './components/exercise-02';
+import Exercise03 from './components/exercise-03';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <table id="idCard">
+      <tr>
+        <td><img  id="avatar" src="NGUYEN NGOC VINH AN.jpg"></img></td>
+        <td id="info">
+          <p>Họ tên: Nguyễn Ngọc Vĩnh An</p>
+          <p>Giới tính: Nữ</p>
+          <p>Địa chỉ: Gò Vấp, TPHCM, Việt Nam</p>
+          <p>Số điện thoại: 0933 633 516</p>
+          <p>Email: ngngvinh.an@gmail.com</p>
+          <p>Facebook/Zalo: Vĩnh An (0933 633 516)</p>
+        </td>
+      </tr>
+    </table>
+    <Exercise02/>
+    <Exercise03/>
+    </>
   );
 }
 
